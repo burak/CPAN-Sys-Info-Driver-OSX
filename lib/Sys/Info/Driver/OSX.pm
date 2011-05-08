e8b3483ffcb74e129df050971ac8c0c942813584
Burak Gürsoy <burak@cpan.org>
2011-05-09 02:08:35 +0300

    Error checks for bitness. Cleanup.

--- lib/Sys/Info/Driver/OSX.pm
+++ lib/Sys/Info/Driver/OSX.pm
@@ -11,7 +11,7 @@ use constant SYSCTL_NOT_EXISTS  =>
 use Capture::Tiny qw( capture );
 use Carp          qw( croak   );
 
-our $VERSION = '0.79';
+our $VERSION = '0.791';
 our @EXPORT  = qw( fsysctl nsysctl sw_vers system_profiler );
 
 sub system_profiler {

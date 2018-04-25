ba232da5e1cface9eff9a70c8b3418a4268d2822
Burak Gursoy <burak@cpan.org>
2018-04-25 18:02:31 +0200

    migrate to dzil

---
+++ check_os.pl
@@ -0,0 +1,10 @@
+
+# BEGIN dynamic
+
+# This section is used/injected by dzil and not to be executed as a
+# standalone program
+
+# copy-paste from Sys::Info::Constants
+die "OS unsupported\n" if $^O ne 'darwin';
+
+# END dynamic
